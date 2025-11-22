baaf2cf381317ca5ed1adede249aa3854e278b4c
m0ck1516 <christian_j_crawford@icloud.com>
2025-11-22 02:52:05 -0600

    Updating the shopping cart page, a milestone update

---
+++ src/frontend/src/components/ui/columns.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import type { CartProduct } from "../../helpers/cart/context.tsx";
+import { ArrowUpDown } from "lucide-react";
+import { Button } from "./button.tsx";
+
+export const columns: ColumnDef<CartProduct>[] = [
+  {
+    accessorKey: "cartQuantity",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          // Clean up button styling in header
+          className="p-0 hover:bg-transparent"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Product
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => (
+      <div className="text-left">{row.original.cartQuantity}</div>
+    ),
+  },
+  {
+    accessorKey: "name",
+    header: () => <div className="text-left">Product</div>,
+    cell: ({ row }) => <div className="font-medium">{row.original.name}</div>,
+  },
+  {
+    accessorKey: "price",
+    header: ({ column }) => {
+      return (
+        <div className="flex justify-end">
+          {" "}
+          {/* Ensures the button is right-aligned in the header */}
+          <Button
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+            className="p-0 hover:bg-transparent"
+          >
+            Price
+            <ArrowUpDown className="ml-2 h-4 w-4" />
+          </Button>
+        </div>
+      );
+    },
+    cell: ({ row }) => (
+      <div className="text-right">${row.original.price.toFixed(2)}</div>
+    ),
+  },
+];
